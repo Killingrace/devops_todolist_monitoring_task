e8c78663c10bb21d70feda73dd77fc098a8bbe88
Dmytro <dima.shutko5@gmail.com>
2026-05-10 15:04:41 +0200

    sol3

--- src/api/views.py
+++ src/api/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets
-from prometheus_client import Counter, generate_latest
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 from api.serializers import TodoListSerializer, TodoSerializer, UserSerializer
 from lists.models import Todo, TodoList
 
 from django.http import HttpResponse
 from django.utils import timezone
-import time
 
 startup_time = timezone.now()
 
@@ -17,38 +16,28 @@ REQUEST_COUNT = Counter(
 )
 
 class IsCreatorOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `creator` attribute.
-    """
-
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # If the object doesn't have a creator (i.e. anon) allow all methods.
         if not obj.creator:
             return True
-
-        # Instance must have an attribute named `creator`.
         return obj.creator == request.user
 
+class InstrumentedViewSet(viewsets.ModelViewSet):
+    def dispatch(self, request, *args, **kwargs):
+        # Використовуємо назву класу для мітки 'view'
+        REQUEST_COUNT.labels(
+            method=request.method, 
+            view=self.__class__.__name__
+        ).inc()
+        return super().dispatch(request, *args, **kwargs)
 
-class UserViewSet(viewsets.ModelViewSet):
-
+class UserViewSet(InstrumentedViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (permissions.IsAdminUser,)
 
-    def dispatch(self, request, *args, **kwargs):
-        REQUEST_COUNT.labels(method=request.method, view='UserViewSet').inc()
-        return super().dispatch(request, *args, **kwargs)
-
-
-class TodoListViewSet(viewsets.ModelViewSet):
-
+class TodoListViewSet(InstrumentedViewSet):
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
     permission_classes = (IsCreatorOrReadOnly,)
@@ -58,9 +47,7 @@ class TodoListViewSet(viewsets.ModelViewSet):
         creator = user if user.is_authenticated else None
         serializer.save(creator=creator)
 
-
-class TodoViewSet(viewsets.ModelViewSet):
-
+class TodoViewSet(InstrumentedViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     permission_classes = (IsCreatorOrReadOnly,)
@@ -70,18 +57,16 @@ class TodoViewSet(viewsets.ModelViewSet):
         creator = user if user.is_authenticated else None
         serializer.save(creator=creator)
 
-# Health Check View
 def health(request):
     REQUEST_COUNT.labels(method='GET', view='health').inc()
     return HttpResponse("Health OK", content_type="text/plain")
 
-# Readiness Check View
 def ready(request):
-    REQUEST_COUNT.labels(method='GET', view='health').inc()
+    REQUEST_COUNT.labels(method='GET', view='ready').inc()
     elapsed_time = timezone.now() - startup_time
     if elapsed_time.total_seconds() < 30:
-        # Return HTTP 500 for the first 30 seconds after startup
         return HttpResponse("Service not ready", status=500, content_type="text/plain")
-    else:
-        # After 30 seconds, return HTTP 200
-        return HttpResponse("Readiness OK", content_type="text/plain")
+    return HttpResponse("Readiness OK", content_type="text/plain")
+
+def metrics(request):
+    return HttpResponse(generate_latest(), content_type=CONTENT_TYPE_LATEST)
--- src/todolist/urls.py
+++ src/todolist/urls.py
@@ -1,3 +1,4 @@
+from api.views import metrics # Не забудьте цей імпорт!
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,5 +8,5 @@ urlpatterns = [
     path("api/", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
-    path("", include('django_prometheus.urls')),
+    path("metrics/", metrics, name="prometheus-metrics"),
 ]
